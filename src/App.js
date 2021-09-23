6531c7449cf8db5ebb7544c959fbbc5d221e2a9a
kareemshono <kareem@showno.co>
2021-09-23 04:10:31 -0700

    first

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <ul>
+          <li><a href='#'>test_link</a></li>
+        </ul>
+      </nav>
+    <p>  Build fast, responsive sites with Bootstrap
+Quickly design and customize responsive mobile-first sites with Bootstrap, 
+the world’s most popular front-end open source toolkit, featuring Sass
+ variables and mixins, responsive grid system, extensive prebuilt components,
+  and powerful JavaScript plugins.</p>
+    </div>
+  );
+}
+
+export default App;
